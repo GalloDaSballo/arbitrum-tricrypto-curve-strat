1e0acae525145ae4b1463afd7f6cbb918555dfe9
Entreprenerd <valeranijr@gmail.com>
2021-09-16 01:47:36 +0200

    fix: multicall issues

--- helpers/multicall/constants.py
+++ helpers/multicall/constants.py
@@ -10,6 +10,9 @@ class Network(IntEnum):
     xDai = 100
     Forknet = 1337
     BSC = 56
+    Polygon = 137
+    Hardhat = 31337
+    Arbitrum = 42161
 
 
 MULTICALL_ADDRESSES = {
@@ -20,4 +23,7 @@ MULTICALL_ADDRESSES = {
     Network.xDai: "0xb5b692a88BDFc81ca69dcB1d924f59f0413A602a",
     Network.Forknet: "0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441",
     Network.BSC: "0xec8c00da6ce45341fb8c31653b598ca0d8251804",
+    Network.Polygon: "0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507",
+    Network.Arbitrum: "0x7A7443F8c577d537f1d8cD4a629d40a3148Dd7ee",
+    Network.Hardhat: "0x7A7443F8c577d537f1d8cD4a629d40a3148Dd7ee",
 }
